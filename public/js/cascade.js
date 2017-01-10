7a30e51be8ad1819850a5824b1ebb8bdbc54d051
Selvin11 <528370630@qq.com>
2017-01-10 14:51:24 +0800

    add cascade.jquery

--- public/js/cascade.js
+++ public/js/cascade.js
@@ -6,15 +6,22 @@ $(function() {
             "createDate": "2016-12-26 22:32:23",
             "modifyDate": "2016-12-26 22:32:47",
             "city": "深圳市",
+            "name": "不限",
+            "site": ""
+        }, {
+            "id": 1,
+            "createDate": "2016-12-26 22:32:23",
+            "modifyDate": "2016-12-26 22:32:47",
+            "city": "深圳市",
             "name": "1号线",
-            "site": "会展中心,岗厦"
+            "site": "不限,会展中心,岗厦"
         }, {
             "id": 2,
             "createDate": "2016-12-26 22:32:52",
             "modifyDate": "2016-12-26 22:33:16",
             "city": "深圳市",
             "name": "4号线",
-            "site": "会展中心,市民中心"
+            "site": "不限,会展中心,市民中心"
         }]
     };
 
@@ -23,10 +30,36 @@ $(function() {
             "billNo": "HA161100001",
             "parentBillNo": "1",
             "areaCode": "NSQ",
+            "areaName": "不限",
+            "path": "SZ.NSQ",
+            "parentName": null,
+            "children": [{
+                "billNo": "HA161100004",
+                "parentBillNo": "HA161100001",
+                "areaCode": "NANTOUPIAN",
+                "areaName": "",
+                "path": "SZ.NSQ.NANTOUPIAN",
+                "parentName": null,
+                "children": []
+            }, {
+                "billNo": "HA161100005",
+                "parentBillNo": "HA161100001",
+                "areaCode": "NANYOU",
+                "areaName": "",
+                "path": "SZ.NSQ.NANYOU",
+                "parentName": null,
+                "children": []
+            }]
+        }, {
+            "billNo": "HA161100001",
+            "parentBillNo": "1",
+            "areaCode": "NSQ",
             "areaName": "南山区",
             "path": "SZ.NSQ",
             "parentName": null,
             "children": [{
+                "areaName": "不限"
+            }, {
                 "billNo": "HA161100004",
                 "parentBillNo": "HA161100001",
                 "areaCode": "NANTOUPIAN",
@@ -43,8 +76,35 @@ $(function() {
                 "parentName": null,
                 "children": []
             }]
+        }, {
+            "billNo": "HA161100001",
+            "parentBillNo": "1",
+            "areaCode": "NSQ",
+            "areaName": "龙岗区",
+            "path": "SZ.NSQ",
+            "parentName": null,
+            "children": [{
+                "areaName": "不限"
+            }, {
+                "billNo": "HA161100004",
+                "parentBillNo": "HA161100001",
+                "areaCode": "NANTOUPIAN",
+                "areaName": "上塘",
+                "path": "SZ.NSQ.NANTOUPIAN",
+                "parentName": null,
+                "children": []
+            }, {
+                "billNo": "HA161100005",
+                "parentBillNo": "HA161100001",
+                "areaCode": "NANYOU",
+                "areaName": "深圳北",
+                "path": "SZ.NSQ.NANYOU",
+                "parentName": null,
+                "children": []
+            }]
         }]
     };
+
 
     // 限制高度，禁止页面滚动
     function disabledScroll() {
@@ -58,6 +118,13 @@ $(function() {
         });
     }
 
+    function ifScroll(target) {
+        if (target.css('display') == "block") {
+            disabledScroll();
+        } else {
+            letScroll();
+        }
+    }
     //  $('.sift-item1')  $('.sift-item2') 清空
     function clearAll() {
         $('.sift-item1').empty();
@@ -67,9 +134,17 @@ $(function() {
     var screenTabcontent = $('.screen-tabcontent'),
         screenItem = $('.screen-item');
     screenTabcontent.hide();
+
+    // 点击增加active
+    function addActive(target) {
+        target.addClass('active').siblings('.g-item').removeClass('active');
+    }
+
     // 第一步 点击筛选栏
 
     screenItem.on('click', function(e) {
+        var that = $(this);
+
         // 点击时，当前索引值
         var screenIndex = $(this).index();
         // 遍历四个tabcontent，索引不为当前值，执行以下函数
@@ -85,14 +160,11 @@ $(function() {
         var siftWrap = screenTabcontent.eq(screenIndex);
         // 第一次点击显示筛选框
         siftWrap.toggle();
-        if (siftWrap.css('display') === "block") {
-            disabledScroll();
-        } else {
-            letScroll();
-        }
+
+        ifScroll(siftWrap);
 
         // 箭头方向
-        var iconfont = $(this).find('.iconfont');
+        var iconfont = that.find('.iconfont');
 
         function arrowDirec() {
             if (iconfont.hasClass('icon-bottom-arrow')) {
@@ -104,16 +176,47 @@ $(function() {
         arrowDirec();
 
         // 第二步 筛选框内容点击更新
-        // 点击增加active
-        function addActive(target) {
-            target.addClass('active').siblings('.item').removeClass('active');
+
+
+
+        //点击不限 刷新数据源
+        var navData = ['位置', '租金', '排序', '居室'];
+        // 一级不限
+        function noLimit1(target, nav, index) {
+            if (target.text() === "不限") {
+                $(screenItem[screenIndex]).find('.name').text(navData[screenIndex]);
+                $('.sift-wrap').hide();
+                ifScroll(siftWrap);
+                arrowDirec();
+            } else {
+                $(screenItem[screenIndex]).find('.name').text(target.text());
+            }
         }
+        // 二级不限
+        function noLimit2(target) {
+            if ($('.sift-item0 .item').hasClass('active') && $('.sift-item1 .item').hasClass('active')) {
+                if (target.text() === "不限") {
+                    $('.sift-item1 .item').each(function() {
+                        if ($(this).hasClass('active')) {
+                            $(screenItem[screenIndex]).find('.name').text($(this).text());
+                        }
+                    })
+                } else {
+                    $(screenItem[screenIndex]).find('.name').text(target.text());
+
+                }
+                $('.sift-wrap').hide();
+                ifScroll(siftWrap);
+                arrowDirec();
+            }
+        }
+
         // 数据刷新
         function update(target, data, dataType, dataName1, dataName2) {
             target.on('click', function() {
                 addActive($(this));
                 clearAll();
-                var names = '<li class="item">不限</li>';
+                var names = '';
                 // 获取第二栏数据
                 $(data[dataType]).each(function(i, v) {
                     names += "<li class='item'>" + v[dataName1] + "</li>";
@@ -122,18 +225,22 @@ $(function() {
                 // 获取第三栏数据
                 $('.sift-item1').find('.item').on('click', function() {
                     addActive($(this));
-                    var sites = '<li class="item">不限</li>',
+                    var sites = '',
                         index = $(this).index(),
                         siteArr = [];
-                    if (dataName2 !== "") {
+                    if (dataName2) {
                         siteArr = data[dataType][index][dataName2].split(",");
-                        $(siteArr).each(function(i, v) {
-                            sites += "<li class='item'>" + v + "</li>";
-                        })
+                        if (siteArr) {
+                            $(siteArr).each(function(i, v) {
+                                sites += "<li class='item'>" + v + "</li>";
+                            })
+                        }
                     } else {
-                        $(data[dataType][index].children).each(function(i, v) {
-                            sites += "<li class='item'>" + v[dataName1] + "</li>";
-                        })
+                        if (data[dataType][index].children[dataName1]) {
+                            $(data[dataType][index].children).each(function(i, v) {
+                                sites += "<li class='item'>" + v[dataName1] + "</li>";
+                            })
+                        }
                         $('.sift-item2').empty().append(sites);
                     }
                     $('.sift-item2').empty().append(sites);
@@ -154,31 +261,31 @@ $(function() {
         $("#subway").on('click', function() {
             addActive($(this));
             clearAll();
-            var names = '<li class="item">不限</li>';
+            var names = '';
             // 获取地铁线路
             $(data.subways).each(function(i, v) {
-                names += "<li class='item'>" + v.name + "</li>";
+                names += "<li class='g-item item'>" + v.name + "</li>";
             });
             $('.sift-item1').empty().append(names);
             // 获取地铁线路对应的站台
             $('.sift-item1').find('.item').on('click', function() {
+                noLimit1($(this));
                 addActive($(this));
-                var sites = '<li class="item">不限</li>',
+                var sites = '',
                     index = $(this).index(),
                     siteArr = [];
                 siteArr = data.subways[index].site.split(",");
-                $(siteArr).each(function(i, v) {
-                    sites += "<li class='item'>" + v + "</li>";
-                })
+                if (siteArr != "") {
+                    $(siteArr).each(function(i, v) {
+                        sites += "<li class='g-item item'>" + v + "</li>";
+                    })
+                }
                 $('.sift-item2').empty().append(sites);
-                // 点击站台刷新房源数据
+                // 点击站台刷新房源数据 点击不限或者选择了三项
+
                 $('.sift-item2').find('.item').on('click', function() {
                     addActive($(this));
-                    if ($('.sift-item0 .item').hasClass('active') && $('.sift-item1 .item').hasClass('active')) {
-                        $(screenItem[screenIndex]).find('.name').text($(this).text());
-                        $('.sift-wrap').hide();
-                        arrowDirec();
-                    }
+                    noLimit2($(this));
                 })
             })
         });
@@ -188,29 +295,29 @@ $(function() {
         $('#area').on('click', function() {
             clearAll();
             addActive($(this));
-            var names = '<li class="item">不限</li>';
+            var names = '';
             // 获取地区
             $(dataArea.areas).each(function(i, v) {
-                names += "<li class='item'>" + v.areaName + "</li>";
+                names += "<li class='g-item item'>" + v.areaName + "</li>";
             });
             $('.sift-item1').empty().append(names);
             // 获取地区对应的区域
             $('.sift-item1').find('.item').on('click', function() {
                 addActive($(this));
-                var sites = '<li class="item">不限</li>',
+                noLimit1($(this));
+                var sites = '',
                     index = $(this).index();
                 $(dataArea.areas[index].children).each(function(i, v) {
-                    sites += "<li class='item'>" + v.areaName + "</li>";
+                    if (v.areaName) {
+                        sites += "<li class='g-item item'>" + v.areaName + "</li>";
+                    }
                 })
                 $('.sift-item2').empty().append(sites);
                 // 点击站台刷新房源数据
+                // 1.
                 $('.sift-item2').find('.item').on('click', function() {
                     addActive($(this));
-                    if ($('.sift-item0 .item').hasClass('active') && $('.sift-item1 .item').hasClass('active')) {
-                        $(screenItem[screenIndex]).find('.name').text($(this).text());
-                        $('.sift-wrap').hide();
-                        arrowDirec();
-                    }
+                    noLimit2($(this));
                 })
             })
         });
@@ -221,19 +328,21 @@ $(function() {
         //  筛选框隐藏条件：1. 三列数据均含.active  && 点击了第三列中的一项 （并刷新房源）
         //                  2. 不满足条件1，点击筛选框之外的地方
 
-        $('.sift-item2 .item.active').on('click', function() {
-            if ($('.sift-item0 .item').hasClass('active') && $('.sift-item1 .item').hasClass('active')) {
-                siftWrap.hide();
-                arrowDirec();
-            }
-        });
+        // $('.sift-item2 .item.active').on('click', function() {
+        //     if ($('.sift-item0 .item').hasClass('active') && $('.sift-item1 .item').hasClass('active')) {
+        //         siftWrap.hide();
+        //         ifScroll(siftWrap);
+        //         arrowDirec();
+        //     }
+        // });
 
         // 租金、排序、居室
         if ($(screenTabcontent[screenIndex]).is('.col-nav-wrap')) {
-            $(screenTabcontent[screenIndex]).find('.col-nav-item').on('click', function() {
+            $(screenTabcontent[screenIndex]).find('.col-nav-item').off('click').on('click', function(e) {
                 addActive($(this));
-                $(screenItem[screenIndex]).find('.name').text($(this).text());
+                noLimit1($(this));
                 $(screenTabcontent[screenIndex]).hide();
+                ifScroll(siftWrap);
                 screenItem.find('.iconfont').removeClass('icon-top-arrow').addClass('icon-bottom-arrow');
             })
         }
@@ -242,8 +351,10 @@ $(function() {
             // 点击筛选框之外的地方
             if ($(e.target).is('.screen-tabcontent')) {
                 $(this).hide();
+                ifScroll($(this));
                 screenItem.find('.iconfont').removeClass('icon-top-arrow').addClass('icon-bottom-arrow');
             }
         });
-    });
+
+    }); //点击四个分栏结束
 })
